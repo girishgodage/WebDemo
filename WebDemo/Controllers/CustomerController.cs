9cdc3a14592e9a49031f49b1838b7d0794fbccf3
girish <girishgodage@gmail.com>
2021-03-21 12:13:20 +0530

    first commit

---
+++ WebDemo/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebDemo.Resources;
+
+namespace WebDemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly LocalizationService _localizationService;
+
+        public CustomerController(LocalizationService localizationService)
+        {
+            _localizationService = localizationService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var fullName = _localizationService.GetLocalizedHtmlString("FullName");
+            var city = _localizationService.GetLocalizedHtmlString("City");
+            var mobileno = _localizationService.GetLocalizedHtmlString("MobileNo");
+
+            return View();
+        }
+    }
+}
